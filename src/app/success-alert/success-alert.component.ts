b0534b7dc512f7752463213611018ffc6f88d534
Matheus Pereira <matheus.spereira@hotmail.com>
2018-09-11 16:45:58 -0300

    initial commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: '<div class="alert alert-success">Success alert</div>'
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: '<div class="alert alert-warning">Warning alert</div>'
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
